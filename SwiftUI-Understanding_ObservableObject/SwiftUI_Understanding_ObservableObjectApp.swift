404334f975cb9767c39b8995309d2d77a76445fe
Arpit Dixit <arpitd@dewsolutions.in>
2021-06-23 20:17:52 +0530

    Initial Commit

---
+++ SwiftUI-Understanding_ObservableObject/SwiftUI_Understanding_ObservableObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Understanding_ObservableObjectApp.swift
+//  SwiftUI-Understanding_ObservableObject
+//
+//  Created by Arpit Dixit on 23/06/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Understanding_ObservableObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
